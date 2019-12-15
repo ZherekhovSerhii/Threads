5cf54236ead8eea7c5af4c8c934bc31d3beb2803
ZherekhovSerhii <55210753+ZherekhovSerhii@users.noreply.github.com>
2019-12-15 02:04:25 +0200

    Multithread vector calculating - some changes

--- multithread_vector.cpp
+++ multithread_vector.cpp
@@ -1,64 +1,69 @@
-#include <future>
 #include <iostream>
-#include <thread>
 #include <vector>
+#include <thread>
+#include <future>
+#include <random>
 
-#include <ctime>
-
-constexpr size_t MY_HW_CONC = 4;
 constexpr size_t VECTOR_SIZE = 1000000;
+const size_t MY_HW_CONC = std::thread::hardware_concurrency();
+std::vector<uint8_t> v(VECTOR_SIZE);
 
-using vector_ptr = std::_Vector_iterator<
-    class std::_Vector_val<struct std::_Simple_types<unsigned char>>>;
+void init() {
+  for (std::vector<uint8_t>::iterator i = v.begin(); i != v.end(); ++i) {
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> distribution(
+        0, std::numeric_limits<uint8_t>::max());
 
-void init(std::vector<uint8_t> &v) {
-  srand(static_cast<unsigned int>(time(0)));
-  for (vector_ptr i = v.begin(); i < v.end(); ++i) {
-    *i = rand() % (std::numeric_limits<uint8_t>::max() + 1);
+    *i = distribution(generator);
   }
 }
 
-void execute_thread(std::vector<uint8_t> &v, vector_ptr begin, vector_ptr end,
+void execute_thread(std::vector<uint8_t>::iterator begin,
+                    const std::vector<uint8_t>::iterator end,
                     std::promise<uint64_t> prom) {
   uint64_t sum = 0;
 
-  for (vector_ptr i = begin; i != end; ++i) {
-    // std::cout << "!" << std::endl;
-    sum += *i;
+  while (begin != end) {
+    sum += *begin++;
   }
 
   prom.set_value(sum);
-  // std::cout << "Exit thread" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-  std::vector<uint8_t> v(VECTOR_SIZE);
-  init(v);
+  init();
 
-  unsigned int start_time = clock();
+  auto begin = std::chrono::system_clock::now();
 
-  std::promise<uint64_t> prom[4];
-  std::future<uint64_t> f[4];
-  for (size_t i = 0; i < 4; ++i) {
+  std::vector<std::promise<uint64_t>> prom(MY_HW_CONC);
+  std::vector<std::future<uint64_t>> f(MY_HW_CONC);
+
+  for (size_t i = 0; i < MY_HW_CONC; ++i)
     f[i] = prom[i].get_future();
-  }
 
-  for (size_t i = 0; i < MY_HW_CONC; ++i) {
-    std::thread t(
-        execute_thread, std::ref(v), v.begin() + (VECTOR_SIZE / MY_HW_CONC) * i,
-        v.begin() + (VECTOR_SIZE / MY_HW_CONC) * (i + 1), std::move(prom[i]));
+  for (size_t i = 0; i < MY_HW_CONC - 1; ++i) {
+    std::thread t(execute_thread, v.begin() + (VECTOR_SIZE / MY_HW_CONC) * i,
+                  v.begin() + (VECTOR_SIZE / MY_HW_CONC) * (i + 1),
+                  std::move(prom[i]));
     // t.join();
     t.detach();
   }
+  std::thread t(execute_thread,
+                v.begin() + (VECTOR_SIZE / MY_HW_CONC) * (MY_HW_CONC - 1),
+                v.end(), std::move(prom.back()));
+  // t.join();
+  t.detach();
+
+  auto end = std::chrono::system_clock::now() - begin;
 
   uint64_t sum = 0;
-  for (size_t i = 0; i < 4; ++i) {
+  for (size_t i = 0; i < MY_HW_CONC; ++i)
     sum += f[i].get();
-  }
 
-  unsigned int end_time = clock();
-  unsigned int search_time = end_time - start_time;
+  std::cout << "SUM= " << sum
+            << "\nTIME: " << std::chrono::duration<double>(end).count()
+            << std::endl;
 
-  std::cout << "SUM= " << sum << "\nTIME: " << search_time << std::endl;
   return 0;
 }
